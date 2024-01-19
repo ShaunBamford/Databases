572551ff449a2313647856402a0418d436638a00
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-01-19 15:55:47 +0000

    Update customer.sql

--- RetailDatabase/customer.sql
+++ RetailDatabase/customer.sql
@@ -6,3 +6,12 @@ CREATE TABLE customers(
     email VARCHAR(255),
     phoneNumber VARCHAR(11)
 );
+
+INSERT INTO customers(firstName,LastName,email,phoneNumber)
+VALUES
+('Fatima', 'Omar','fatimaomar24@gmail.com','07568395723'),
+('Steven','Ogg','iamnottrevor@yahoomail.com','07849563819'),
+('Gary', 'Neville','garyneville@skysports.com','07864546781'),
+('Bob', 'Williams','bobwilliams3456@msn.com','078906545611'),
+('Carter', 'Peter','ilovemoney47@gmail.com','02087637864'),
+('Ramesh','Singh','singhramesh59@gmail.com','01914568976');
