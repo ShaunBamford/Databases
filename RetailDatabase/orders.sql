0b13d58dc49ef5059e1d357c5729d931c936574e
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-01-19 15:36:00 +0000

    Update orders.sql

--- RetailDatabase/orders.sql
+++ RetailDatabase/orders.sql
@@ -1,1 +1,8 @@
-
+/* Create a table called Orders that will hold order data */
+CREATE TABLE orders(
+    orderID SERIAL PRIMARY KEY,
+    customerID INTEGER,
+    orderDate DATE,
+    productID INTEGER,
+    quantity INTEGER
+);
