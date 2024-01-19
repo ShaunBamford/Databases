3e28e800ad4be64238b6c3c74bb5f6ea90ca56fd
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-01-19 15:36:40 +0000

    Update employees.sql

--- RetailDatabase/employees.sql
+++ RetailDatabase/employees.sql
@@ -1,1 +1,9 @@
-
+/* Create a table called Employees that will hold employee data */
+CREATE TABLE employees(
+    employeeID SERIAL PRIMARY KEY,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    department VARCHAR(255),
+    managerID INTEGER,
+    projectID INTEGER  
+);
