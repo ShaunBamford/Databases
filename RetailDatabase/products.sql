e58ba09d27a630e87f42b4158c6b0ed2b175436e
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-01-19 15:55:30 +0000

    Update products.sql

--- RetailDatabase/products.sql
+++ RetailDatabase/products.sql
@@ -6,3 +6,12 @@ CREATE TABLE products(
     price FLOAT,
     stockQuantity INTEGER
 );
+
+INSERT INTO products(productName,productDescription,price,stockQuantity)
+VALUES
+('Lurpack Butter','Lovely, well not really, butter made somewhere',6.70,56),
+('Full-fat Milk','Proper milk for Proper People',1.50,23)
+('16 Free-range Eggs','Eat it, drink it, scramble it, do whatever you want',1.99,77)
+('Pepporoni Pizza','Margherita, Tomato and Pepporoni, all the stuff you need for a good dinner',2.50,7)
+('24 bottles of Water','Water. Essential to survive',2.00,5)
+('Bottle of Bleach','For anything an irish man, or woman, or whatever Brother Fessel is now',5.00,12)
