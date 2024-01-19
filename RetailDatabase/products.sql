368c74f7d0dd99bfd7c4a1b5f93ca15b24cc0f89
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-01-19 15:36:21 +0000

    Update products.sql

--- RetailDatabase/products.sql
+++ RetailDatabase/products.sql
@@ -1,1 +1,8 @@
-
+/* Create a table called Products that will hold product data */
+CREATE TABLE products(
+    productID SERIAL PRIMARY KEY,
+    productName VARCHAR(255),
+    productDescription VARCHAR(255),
+    price FLOAT,
+    stockQuantity INTEGER
+);
