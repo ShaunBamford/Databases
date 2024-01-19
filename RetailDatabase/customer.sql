3b3c8dec526e0504aa25761eac07a36e85308e7c
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-01-19 15:35:41 +0000

    Update customer.sql

--- RetailDatabase/customer.sql
+++ RetailDatabase/customer.sql
@@ -1,1 +1,8 @@
-R
+/* Create a table called Customers that will hold customers data */
+CREATE TABLE customers(
+    customerID SERIAL PRIMARY KEY,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(255),
+    phoneNumber VARCHAR(11)
+);
