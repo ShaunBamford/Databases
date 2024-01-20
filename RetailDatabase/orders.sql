dbc72bd14fa7b22503535454602dbc6067b88fc3
Shaun <151833832+ShaunBamford@users.noreply.github.com>
2024-01-20 14:10:15 +0000

    Update orders.sql

--- RetailDatabase/orders.sql
+++ RetailDatabase/orders.sql
@@ -6,3 +6,12 @@ CREATE TABLE orders(
     productID INTEGER,
     quantity INTEGER
 );
+
+INSERT INTO orders(customerID,orderDate,productID,quantity)
+VALUES
+(3,'2024-01-15',3,1),
+(6,'2024-01-19',2,5),
+(2,'2024-01-13',5,1),
+(4,'2024-01-12',1,12),
+(1,'2024-01-20',6,6)
+(1,'2024-01-20',4,2)
